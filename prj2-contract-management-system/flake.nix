24d6207d09ed47397a96589d638c248bf796fe43
AzurIce <973562770@qq.com>
2023-04-07 12:08:50 +0800

    Created flake

---
+++ prj2-contract-management-system/flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+        ];
+
+        packages = with pkgs; [
+            statik
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            '';
+        };
+      });
+}
