b95df77c5fb69e356fe9c8285ec2441996a9bb4a
Azur冰弦 <973562770@qq.com>
2023-04-09 12:25:49 +0800

    prj2: added unocss-preset-flowbite

--- prj2-contract-management-system/prj2-frontend/uno.config.ts
+++ prj2-contract-management-system/prj2-frontend/uno.config.ts
@@ -1,10 +1,12 @@
 // uno.config.ts
 import { defineConfig, presetIcons, presetUno } from 'unocss';
+import { presetFlowbite } from '@julr/unocss-preset-flowbite'
 
 export default defineConfig({
   // ...UnoCSS options
   presets: [
     presetUno(),
+    presetFlowbite(),
     presetIcons({ /* options */ }),
   ],
 })
