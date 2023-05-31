d98d8f725b7e1ebd42f5432f1ffc1a4ea5151cec
AzurIce <973562770@qq.com>
2023-05-31 20:21:18 +0800

    basic 2pl logic

--- 2pl/main.go
+++ 2pl/main.go
@@ -3,113 +3,54 @@ package main
 import (
 	"log"
 	"math/rand"
+	"sync"
 	"time"
+
+    "github.com/faith/color"
 )
 
-type TransactionType int
-
-const (
-    ReadType TransactionType = iota
-    WriteType
-)
-
-func (t TransactionType) String() string {
-    switch t {
-    case ReadType:
-        return "ReadType"
-    case WriteType:
-        return "WriteType"
-    }
-    return "Unknown"
-}
-
-type transaction struct {
-    Id        int
-    Target    int
-    Type      int
-    Second    int
-    startChan chan int
-}
-
+// for start signal
 var start chan int
+var wg sync.WaitGroup
 
-func (t *transaction) PrepareToRun() {
-    <- start
+func (t *Transaction) WaitToStart() {
+    wg.Add(1)
+    <- start // Will block until start channel is closed
     log.Printf("Transaction %v starts\n", t.Id)
 
-    scheduerReqChan <- t
+    for _, task := range t.Tasks {
+        scheduerReqChan <- task
 
-    <- t.startChan
-    time.Sleep(time.Duration(t.Second) * time.Second)
-    scheduerDoneChan <- t
+        <- task.StartChan // Will block until it's closed
+        log.Printf("%v starts", task)
+        time.Sleep(task.Time)
+        scheduerDoneChan <- task
+    }
+
+    log.Printf("Transaction %v done\n", t.Id)
+    wg.Done()
 }
 
-var scheduerReqChan chan *transaction
-var scheduerDoneChan chan *transaction
+var scheduerReqChan chan *Task
+var scheduerDoneChan chan *Task
 
 func init() {
     start = make(chan int)
-    scheduerReqChan = make(chan *transaction)
-    scheduerDoneChan = make(chan *transaction)
+    scheduerReqChan = make(chan *Task)
+    scheduerDoneChan = make(chan *Task)
 }
 
 func scheduer() {
-    lockTableHeld := [64][]*transaction{};
-    lockTableReq  := [64][]*transaction{};
-    log.Println(len(lockTableHeld[0]))
-    log.Println(len(lockTableReq[0]))
+    // lockTableHeld := [64][]Task{};
+    // lockTableReq  := [64][]Task{};
+
     for {
         select {
-        case transaction := <- scheduerReqChan:
-            target := transaction.Target
-            log.Printf("Recieved request from %v: ", target)
-
-            allRead := true
-            for _, tr := range lockTableHeld[target] {
-                if tr.Type == int(WriteType) {
-                    allRead = false
-                    break
-                }
-            }
-            if allRead {
-                added := false
-                for i, tr := range lockTableHeld[target] {
-                    if tr == nil {
-                        lockTableHeld[target][i] = transaction
-                        added = true
-                    }
-                }
-                if !added {
-                    lockTableHeld[target] = append(lockTableHeld[target], transaction)
-                }
-                close(transaction.startChan)
-            } else {
-                added := false
-                for i, tr := range lockTableHeld[target] {
-                    if tr == nil {
-                        lockTableReq[target][i] = transaction
-                        added = true
-                    }
-                }
-                if !added {
-                    lockTableReq[target] = append(lockTableReq[target], transaction)
-                }
-            }
-        case transaction := <- scheduerDoneChan:
-            target := transaction.Target
-            id := transaction.Id
-            log.Printf("Recieved done from %v: ", target)
-
-            for i, tr := range lockTableHeld[target] {
-                if tr.Id == id {
-                    lockTableHeld[target][i] = nil
-                }
-            }
-            if len(lockTableHeld[transaction.Target]) == 0 {
-                lockTableHeld[transaction.Target] = append(lockTableHeld[transaction.Target], transaction)
-            } else {
-                lockTableReq[transaction.Target] = append(lockTableReq[transaction.Target], transaction)
-            }
+        case task := <- scheduerReqChan:
+            log.Printf("[scheduer]: Get task request: %v\n", task)
+            close(task.StartChan)
+        case task := <- scheduerDoneChan:
+            log.Printf("[scheduer]: Get task done: %v\n", task)
         }
     }
 }
@@ -117,17 +58,12 @@ func scheduer() {
 func main() {
     rand.Seed(0)
 
-    go scheduer()
-    for i := 1; i <= 100; i++{
-        genTransaction := transaction{
-            i,
-            rand.Int() % 64,
-            rand.Int() % 2,
-            rand.Int() % 5,
-            make(chan int),
-        }
-        go genTransaction.PrepareToRun()
-        log.Printf("Generated Transaction: %#v\n", genTransaction)
+    for i := 1; i <= 10; i++{
+        genTransaction := RandTransaction(i)
+        go genTransaction.WaitToStart()
+        log.Printf("Generated Transaction: %v\n", genTransaction)
     }
     close(start)
+    go scheduer()
+    wg.Wait()
 }
---
+++ 2pl/transaction.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const (
+    MaxTarget = 64
+    MaxTask = 3
+    MaxTime = 3
+)
+
+// LockType -> read | write
+type LockType int
+
+const (
+    ReadType LockType = iota
+    WriteType
+)
+
+func (t LockType) String() string {
+    switch t {
+    case ReadType:
+        return "ReadType"
+    case WriteType:
+        return "WriteType"
+    }
+    return "Unknown"
+}
+
+func RandLockType() LockType {
+    return LockType(rand.Int() % 2);
+}
+
+// Task
+type Task struct {
+    Id     int
+    Target int
+    Type   LockType
+    Time   time.Duration
+    StartChan chan int
+}
+
+func (t *Task) String() string {
+    return fmt.Sprintf("Task<Id=%v, Target=%v, Type=%v, Time=%v>", t.Id, t.Target, t.Type, t.Time)
+}
+
+func RandTask(id int) *Task {
+    return &Task{
+        id,
+        rand.Int() % MaxTarget,
+        RandLockType(),
+        time.Duration(rand.Int() % MaxTime + 1) * time.Second,
+        make(chan int),
+    }
+}
+
+// Transaction
+type Transaction struct {
+    Id        int
+    Tasks     []*Task
+    StartChan chan int
+}
+
+func (t *Transaction) String() string {
+    taskStrs := []string{}
+    for _, task := range t.Tasks {
+        taskStrs = append(taskStrs, task.String())
+    }
+    tasksStr := strings.Join(taskStrs, ",")
+    res := fmt.Sprintf("Transaction<Id=%v, Tasks=[%v]>", t.Id, tasksStr)
+    return res
+}
+
+func RandTransaction(id int) *Transaction {
+    tasks := []*Task{};
+    taskNum := rand.Int() % MaxTask + 1;
+    for i := 1; i <= taskNum; i++ {
+        tasks = append(tasks, RandTask(i))
+    }
+
+    return &Transaction{
+        id,
+        tasks,
+        make(chan int),
+    }
+}
+
